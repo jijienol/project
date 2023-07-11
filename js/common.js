1ed6df824931e86b7afc12d355be514468cbf82b
jijie <2713193571@qq.com>
2023-07-11 19:52:45 +0800

    注册登录页面完成

--- js/common.js
+++ js/common.js
@@ -14,4 +14,18 @@ function getToast(msg){
   document.querySelector('.toast-body').innerHTML = msg
       toast.show()
 }
+
+ function verify(ele, reg, msg) {
+      const result = reg.test(ele.value)
+      const toast = new bootstrap.Toast(document.querySelector('#myToast', {
+        animation: true,
+        autohide: true,
+        delay: 3000
+      }))
+      if(!result){
+        document.querySelector('.toast-body').innerHTML = msg
+        toast.show()
+      }
+      return result
+    }
 axios.defaults.baseURL = 'http://ajax-api.itheima.net'
