07739ccf7fe02c651c974c7207674bcecf75a211
jijie <2713193571@qq.com>
2023-07-11 17:41:52 +0800

    注册功能完成

--- js/common.js
+++ js/common.js
@@ -5,3 +5,13 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('ready')
   }, 200)
 })
+function getToast(msg){
+   const toast = new bootstrap.Toast(document.querySelector('#myToast', {
+      animation: true,
+      autohide: true,
+      delay: 3000
+    }))
+  document.querySelector('.toast-body').innerHTML = msg
+      toast.show()
+}
+axios.defaults.baseURL = 'http://ajax-api.itheima.net'
